5f4ac265ae47c57e4bf8d68246435c7ee3d4a309
diogofalken <diogofalken0@gmail.com>
2020-02-01 13:38:25 +0000

    fix: error when bot was replying to messages outside NSFW channel and added timestamp to log to better understanding of the commands and when they were executed

--- src/main.js
+++ src/main.js
@@ -1,39 +1,45 @@
 const Discord = require('discord.js');
 const { prefix, nswfChannel } = require('./config.json');
-const nsfw = require('./nsfw.js');
+const Nsfw = require('./nsfw.js');
+const Utils = require('./utils.js');
 const http = require('http');
 const port = 3000;
 
 // This is a simple server
 http.createServer().listen(port);
 
-const client = new Discord.Client();
-const NSFW = new nsfw();
+const bot = new Discord.Client();
+const nsfw = new Nsfw();
+const utils = new Utils();
 
-client.once('ready', () => {
-  console.log('LOG: Cygnus ready to go');
+bot.once('ready', () => {
+  console.log(`${utils.getTimestamp()} - Cygnus ready to go`);
 });
 
-client.on('message', message => {
+bot.on('message', message => {
   const args = message.content.slice(prefix.length).split(' ')[1];
 
-  if (message.channel.id != nswfChannel) return;
+  if (message.channel.name != nswfChannel) {
+    console.log(`${utils.getTimestamp()} - Wrong chat don't listen`);
+    return;
+  }
 
   if (message.content.startsWith(`${prefix}real`)) {
-    console.log('LOG: Cygnus !real');
-    NSFW.real(args, message);
+    console.log(`${utils.getTimestamp()} - Cygnus !real`);
+    nsfw.real(args, message);
   } else if (message.content.startsWith(`${prefix}hentai`)) {
-    console.log('LOG: Cygnus !hentai');
-    NSFW.hentai(args, message);
+    console.log(`${utils.getTimestamp()} - Cygnus !hentai`);
+    nsfw.hentai(args, message);
   } else if (message.content.startsWith(`${prefix}help`)) {
+    console.log(`${utils.getTimestamp()} - Cygnus !help`);
     message.reply(
       '**:shinto_shrine: Cygnus - Cheat Sheet**    ** :peach: NSFW commands:**    ```!real {ass,feet,thighs,random}``` ```!hentai {ass,feet,thighs,feet}```'
     );
   }
 });
 
-client.on('error', err => {
+bot.on('error', err => {
   console.log(err);
 });
 
-client.login(process.env.token);
+bot.login(process.env.token);
--- src/nsfw.js
+++ src/nsfw.js
@@ -2,60 +2,64 @@ const DabiImages = require('dabi-images');
 
 const DabiClient = new DabiImages.Client();
 
-function nsfw() {
-    this.real = (arg, message) => {
-        switch (arg) {
-            case "ass":
-                DabiClient.nsfw.real.ass().then(json => {
-                    message.channel.send(json.url)
-                })
-                break;
-            case "thighs":
-                DabiClient.nsfw.real.thighs().then(json => {
-                    message.channel.send(json.url)
-                })
-                break;
-            case "panties":
-                DabiClient.nsfw.real.panties().then(json => {
-                    message.channel.send(json.url)
-                })
-                break;
-            case "random":
-                DabiClient.nsfw.real.random().then(json => {
-                    message.channel.send(json.url)
-                })
-                break;
-            default:
-                message.reply(`You are out of luck ${arg.toUpperCase()} category doesn't exist :joy:`)
-        }
+function Nsfw() {
+  this.real = (arg, message) => {
+    switch (arg) {
+      case 'ass':
+        DabiClient.nsfw.real.ass().then(json => {
+          message.channel.send(json.url);
+        });
+        break;
+      case 'thighs':
+        DabiClient.nsfw.real.thighs().then(json => {
+          message.channel.send(json.url);
+        });
+        break;
+      case 'panties':
+        DabiClient.nsfw.real.panties().then(json => {
+          message.channel.send(json.url);
+        });
+        break;
+      case 'random':
+        DabiClient.nsfw.real.random().then(json => {
+          message.channel.send(json.url);
+        });
+        break;
+      default:
+        message.reply(
+          `You are out of luck ${arg.toUpperCase()} category doesn't exist :joy:`
+        );
     }
+  };
 
-    this.hentai = (arg, message) => {
-        switch (arg) {
-            case "ass":
-                DabiClient.nsfw.hentai.ass().then(json => {
-                    message.channel.send(json.url)
-                })
-                break;
-            case "thighs":
-                DabiClient.nsfw.hentai.thighs().then(json => {
-                    message.channel.send(json.url)
-                })
-                break;
-            case "panties":
-                DabiClient.nsfw.hentai.panties().then(json => {
-                    message.channel.send(json.url)
-                })
-                break;
-            case "feet":
-                DabiClient.nsfw.hentai.feet().then(json => {
-                    message.channel.send(json.url)
-                })
-                break;
-            default:
-                message.reply(`You are out of luck ${arg.toUpperCase()} category doesn't exist :joy:`)
-        }
+  this.hentai = (arg, message) => {
+    switch (arg) {
+      case 'ass':
+        DabiClient.nsfw.hentai.ass().then(json => {
+          message.channel.send(json.url);
+        });
+        break;
+      case 'thighs':
+        DabiClient.nsfw.hentai.thighs().then(json => {
+          message.channel.send(json.url);
+        });
+        break;
+      case 'panties':
+        DabiClient.nsfw.hentai.panties().then(json => {
+          message.channel.send(json.url);
+        });
+        break;
+      case 'feet':
+        DabiClient.nsfw.hentai.feet().then(json => {
+          message.channel.send(json.url);
+        });
+        break;
+      default:
+        message.reply(
+          `You are out of luck ${arg.toUpperCase()} category doesn't exist :joy:`
+        );
     }
+  };
 }
 
-module.exports = nsfw;
+module.exports = Nsfw;
