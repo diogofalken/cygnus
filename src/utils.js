c62b69130c999528760fd5b700ee29128ac0b25d
diogofalken <diogofalken0@gmail.com>
2020-02-02 22:42:02 +0000

    feat: added two new function to help implement a new feature about getting ementa of the day

--- src/utils.js
+++ src/utils.js
@@ -11,6 +11,57 @@ function Utils() {
       today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
     return date + ' ' + time;
   };
+
+  this.transformDay = (day) => {
+    if (day < 10) { 
+        day = '0' + day; 
+    }
+    return day;
+}
+
+this.transformMonth = (month) => {
+    let monthName = "";
+
+    switch (month) {
+        case '1':
+            monthName = "janeiro";
+            break;
+        case '2':
+            monthName = "fevereiro";
+            break;
+        case '3':
+            monthName = "março";
+            break;
+        case '4':
+            monthName = "abril";
+            break;
+        case '5':
+            monthName = "maio";
+            break;
+        case '6':
+            monthName = "junho";
+            break;
+        case '7':
+            monthName = "julho";
+            break;
+        case '8':
+            monthName = "agosto";
+            break;
+        case '9':
+            monthName = "setembro";
+            break;
+        case '10':
+            monthName = "outubro";
+            break;
+        case '11':
+            monthName = "novembro";
+            break;
+        case '12':
+            monthName = "dezembro";
+            break;
+    }
+    return monthName;
+}
 }
 
 module.exports = Utils;
