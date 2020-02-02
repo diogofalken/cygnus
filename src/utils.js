d83d2fb7081b9ba1d614f7feab557132c4746546
diogofalken <diogofalken0@gmail.com>
2020-02-02 23:06:25 +0000

    feat: added a new function to return full date

--- src/utils.js
+++ src/utils.js
@@ -1,67 +1,75 @@
 function Utils() {
-  this.getTimestamp = () => {
-    const today = new Date();
-    const date =
-      today.getFullYear() +
-      '-' +
-      (today.getMonth() + 1) +
-      '-' +
-      today.getDate();
-    const time =
-      today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
-    return date + ' ' + time;
-  };
+    this.getDate = () => {
+        const today = new Date();
 
-  this.transformDay = (day) => {
-    if (day < 10) { 
-        day = '0' + day; 
+        const date =
+            today.getDate() +
+            '-' +
+            (today.getMonth() + 1) +
+            '-' +
+            today.getFullYear();
+
+        return date;
     }
-    return day;
-}
 
-this.transformMonth = (month) => {
-    let monthName = "";
+    this.getTimestamp = () => {
+        const today = new Date();
+        const date = this.getDate();
+        const time =
+            today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+        return date + ' ' + time;
+    };
 
-    switch (month) {
-        case '1':
-            monthName = "janeiro";
-            break;
-        case '2':
-            monthName = "fevereiro";
-            break;
-        case '3':
-            monthName = "março";
-            break;
-        case '4':
-            monthName = "abril";
-            break;
-        case '5':
-            monthName = "maio";
-            break;
-        case '6':
-            monthName = "junho";
-            break;
-        case '7':
-            monthName = "julho";
-            break;
-        case '8':
-            monthName = "agosto";
-            break;
-        case '9':
-            monthName = "setembro";
-            break;
-        case '10':
-            monthName = "outubro";
-            break;
-        case '11':
-            monthName = "novembro";
-            break;
-        case '12':
-            monthName = "dezembro";
-            break;
+    this.transformDay = (day) => {
+        if (day < 10) {
+            day = '0' + day;
+        }
+        return day;
     }
-    return monthName;
-}
+
+    this.transformMonth = (month) => {
+        let monthName = "";
+
+        switch (month) {
+            case '1':
+                monthName = "janeiro";
+                break;
+            case '2':
+                monthName = "fevereiro";
+                break;
+            case '3':
+                monthName = "março";
+                break;
+            case '4':
+                monthName = "abril";
+                break;
+            case '5':
+                monthName = "maio";
+                break;
+            case '6':
+                monthName = "junho";
+                break;
+            case '7':
+                monthName = "julho";
+                break;
+            case '8':
+                monthName = "agosto";
+                break;
+            case '9':
+                monthName = "setembro";
+                break;
+            case '10':
+                monthName = "outubro";
+                break;
+            case '11':
+                monthName = "novembro";
+                break;
+            case '12':
+                monthName = "dezembro";
+                break;
+        }
+        return monthName;
+    }
 }
 
 module.exports = Utils;
